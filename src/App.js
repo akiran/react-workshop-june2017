a73d62859a8f8ba5bb8ddc238f8e3c37058fae03
kiran <kiran@neostack.com>
2017-06-10 07:25:34 +0530

    rendering todos header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'todomvc-app-css/index.css'
+import 'todomvc-common/base.css'
+import 'todomvc-common/base.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1 className="header">todos</h1>
       </div>
     );
   }
